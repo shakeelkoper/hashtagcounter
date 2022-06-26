09f5d66c63764bf7019f2d1c80cef843e5f4f93b
shakeelkoper <shakeelkoper@gmail.com>
2022-06-26 22:10:17 +0530

    final commit

--- hashcounter.js
+++ hashcounter.js
@@ -1,18 +1,11 @@
-const input = "#a bc #xyz";
 function hashTagCounter() {
   const userInput = document.getElementById("textarea").value
   const myArray = userInput.split(" ");
   const final_list = myArray.filter(checkHasTag);
-  // console.log(final_list);
-  // console.log(final_list.length);
   const length = document.getElementById("count")
-  length.innerHTML = final_list.length
-
-  // const tags = document.getElementById("validTags")
-  // tags.innerHTML = final_list
+  length.innerHTML = final_list.length;
 }
 
 function checkHasTag(item) {
-  console.log(item);
   return item[0] == "#" && item.length > 1 && item[1] != "#";
 }
